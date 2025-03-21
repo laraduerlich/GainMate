d86b466aa1e36d65eeb6d8c54e998f268bfbc60d
Lara <laraduerlich@outlook.com>
2025-03-21 16:43:19 +0100

    test: add tests for Controller and Service

--- backend/src/test/java/com/example/backend/controller/ExerciseControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/ExerciseControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -22,14 +23,34 @@ class ExerciseControllerTest {
     private ExerciseRepo repo;
 
     // --------------------------------------- GET ALL ---------------------------------------
-
-   @Test
-   void getAllExcercises_shouldReturnEmptyList_whenCalledInitially () throws Exception {
+    @Test
+    void getAllExcercises_shouldReturnEmptyList_whenCalledInitially () throws Exception {
        // WHEN & THEN
        mockMvc.perform(get("/api/exercise/all"))
                .andExpect(status().isOk())
                .andExpect(content().json("[]"));
    }
 
-
+   // --------------------------------------- CREATE ---------------------------------------
+    @Test
+    void createExercise_shouldReturnExcercise_whenCalledWithDTO () throws Exception {
+       // WHEN & THEN
+       mockMvc.perform(post("/api/exercise/new")
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .content("""
+                               {
+                                   "name": "Push-Test",
+                                   "note": "Do 3 sets of 15 reps"
+                               }
+"""
+                       ))
+               .andExpect(status().isOk())
+               .andExpect(content().json("""
+                       {
+                           "name": "Push-Test",
+                           "note": "Do 3 sets of 15 reps",
+                           "progressList": null
+                       }
+"""));
+   }
 }
--- backend/src/test/java/com/example/backend/service/ExerciseServiceTest.java
+++ backend/src/test/java/com/example/backend/service/ExerciseServiceTest.java
@@ -1,19 +1,22 @@
 package com.example.backend.service;
 
 import com.example.backend.model.Exercise;
+import com.example.backend.model.ExerciseDTO;
 import com.example.backend.repo.ExerciseRepo;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ExerciseServiceTest {
 
-    private ExerciseRepo exerciseRepo = mock(ExerciseRepo.class);
-    private IdService idService = mock(IdService.class);
+    private final ExerciseRepo exerciseRepo = mock(ExerciseRepo.class);
+    private final IdService idService = mock(IdService.class);
 
     // --------------------------------------- GET ALL ---------------------------------------
 
@@ -30,4 +33,41 @@ class ExerciseServiceTest {
         assertEquals(expected, actual);
     }
 
+    // --------------------------------------- CREATE ---------------------------------------
+
+    @Test
+    void createExercise_shouldCreateExercise_whenCalled() {
+        // GIVEN
+        ExerciseService exerciseService = new ExerciseService(exerciseRepo, idService);
+        ExerciseDTO newExercise = new ExerciseDTO("Test", "Test");
+        Exercise expected = Exercise.builder()
+                .id("1")
+                .name("Test")
+                .note("Test")
+                .build();
+        when(idService.generateId()).thenReturn("1");
+        when(exerciseRepo.save(expected)).thenReturn(expected);
+
+        // WHEN
+        Exercise actual = exerciseService.createExercise(newExercise);
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void createExercise_shouldThrowException_whenExerciseAlreadyExists() {
+        // GIVEN
+        ExerciseService exerciseService = new ExerciseService(exerciseRepo, idService);
+        ExerciseDTO newExercise = new ExerciseDTO("Test", "Test");
+        when(exerciseRepo.existsByName("Test")).thenReturn(true);
+
+        // WHEN & THEN
+        try {
+            exerciseService.createExercise(newExercise);
+            fail("An exception is expected, but none is thrown!");
+        } catch (Exception e) {
+            assertEquals("Exercise already exists!", e.getMessage());
+        }
+    }
 }
