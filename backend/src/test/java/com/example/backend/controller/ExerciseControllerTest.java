af1eecab6b0028eb9483cf2d3ba9a8f438aa647d
Lara <laraduerlich@outlook.com>
2025-03-25 13:59:23 +0100

    test: add tests for ExerciseController and ExerciseService

--- backend/src/test/java/com/example/backend/controller/ExerciseControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/ExerciseControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Exercise;
 import com.example.backend.repo.ExerciseRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,30 @@ class ExerciseControllerTest {
                .andExpect(content().json("[]"));
    }
 
-   // --------------------------------------- CREATE ---------------------------------------
+    // --------------------------------------- GET BY ID -------------------------------------
+    @Test
+    void getExerciseById_shouldReturnExercise_whenCalledWithValidId () throws Exception {
+        // GIVEN
+        Exercise exercise = Exercise.builder()
+                .id("1")
+                .name("Test")
+                .note("Notes")
+                .build();
+        repo.save(exercise);
+
+        // WHEN & THEN
+        mockMvc.perform(get("/api/exercise/" + exercise.id()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            id: "1",
+                            name: "Test",
+                            note: "Notes"
+                        }
+                        """));
+    }
+
+    // --------------------------------------- CREATE ----------------------------------------
     @Test
     void createExercise_shouldReturnExercise_whenCalledWithDTO () throws Exception {
        // WHEN & THEN
@@ -47,10 +71,10 @@ class ExerciseControllerTest {
                .andExpect(status().isOk())
                .andExpect(content().json("""
                        {
-                           "name": "Push-Test",
-                           "note": "Do 3 sets of 15 reps",
-                           "progressList": null
+                          "name": "Push-Test",
+                          "note": "Do 3 sets of 15 reps",
+                          "progressList": null
                        }
-"""));
+                        """));
    }
 }
