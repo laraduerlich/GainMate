74aff709611602f9dae687fee79d47e15c957e53
Lara <laraduerlich@outlook.com>
2025-04-01 15:31:30 +0200

    feat: add WorkoutRunPage.tsxt to be able to start a workout and note down the progress

---
+++ frontend/src/pages/workout/WorkoutRunPage.tsx
@@ -0,0 +1,55 @@
+import {Workout} from "../../types/Workout.tsx";
+import {Exercise} from "../../types/Exercise.tsx";
+import {AxiosResponse} from "axios";
+import {useEffect} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import List from "../../components/List.tsx";
+import ButtonWithIcon from "../../components/ButtonWithIcon.tsx";
+
+type WorkoutRunProps = {
+    workout: Workout | undefined,
+    exercises: Exercise[],
+    getWorkoutById: (id: string) => Promise<AxiosResponse>,
+}
+
+export default function WorkoutRunPage({workout, exercises, getWorkoutById}: WorkoutRunProps) {
+
+    const {id} = useParams<{id: string}>()
+    const navigate = useNavigate()
+
+    // exercises form the workout
+    const openExerciseList: Exercise[] = exercises.filter((exercise: Exercise) =>
+        exercise.id !== undefined && workout?.exerciseIdList.includes(exercise.id)
+    )
+
+    // button handler
+    const handleStartButtonClick = (exerciseId: string | undefined) => {
+        if (exerciseId !== undefined) {
+            navigate("/workout/"+ id + "/exercise/" + exerciseId)
+        }
+    }
+
+    const handleFinishedButtonClick = () => {
+        navigate("/workouts")
+    }
+
+    // Load workout
+    useEffect(() => {
+        if (id !== undefined){
+            getWorkoutById(id)
+        }
+
+    }, [id]);
+
+    return (
+        <>
+            <div>
+                {workout ? workout.name : ""}
+            </div>
+            <div>
+                <List elements={openExerciseList} use={"inWorkout"} handelButtonClick={handleStartButtonClick} />
+            </div>
+            <ButtonWithIcon icon={"finish"} type={"button"} onClick={handleFinishedButtonClick}/>
+        </>
+    )
+}
