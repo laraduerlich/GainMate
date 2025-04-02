e76d2cc346f2888b7a2f32815dd9e00182e430f8
Lara <laraduerlich@outlook.com>
2025-04-02 12:08:49 +0200

    feat: add back button on WorkoutCreatePage.tsx

--- frontend/src/pages/workout/WorkoutCreatePage.tsx
+++ frontend/src/pages/workout/WorkoutCreatePage.tsx
@@ -1,6 +1,8 @@
 import WorkoutCreateForm from "../../components/workout/WorkoutCreateForm.tsx";
 import {Exercise} from "../../types/Exercise.tsx";
 import {Workout} from "../../types/Workout.tsx";
+import {useNavigate} from "react-router-dom";
+import ButtonWithIcon from "../../components/ButtonWithIcon.tsx";
 
 type WorkoutCreateProps = {
     createWorkout: (workout: Workout) => void
@@ -9,9 +11,17 @@ type WorkoutCreateProps = {
 
 export default function WorkoutCreatePage({createWorkout, exercises}: WorkoutCreateProps) {
 
+    const navigate = useNavigate()
+
+    // button handler
+    const handleBackButton = () => {
+        navigate("/workouts");
+    }
+
     return (
         <>
             <WorkoutCreateForm createWorkout={createWorkout} exercises={exercises}/>
+            <ButtonWithIcon icon={"back"} type={"button"} onClick={handleBackButton}/>
         </>
     )
 }
