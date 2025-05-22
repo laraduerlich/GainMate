48ea4f1f1c0cb2a150cdcaa352b061c6ce7608c3
Lara <laraduerlich@outlook.com>
2025-05-22 12:45:44 +0200

    feat: add Name to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,13 +7,18 @@ export default function Header({toggleSidebar}: HeaderProps) {
 
     return (
         <>
-            <div className="w-full max-w-md px-4 py-2 bg-zinc-800 shadow-md  fixed top-0">
+            <div className="fixed top-0 bg-zinc-800 shadow-md px-4 py-2 w-full flex justify-center">
+                <div className="flex items-center gap-x-2 max-w-md">
                 <button
                     type={"button"}
                     onClick={toggleSidebar}
                 >
                     <img src={"/Logo.png"} alt="icon" className="w-13 h-13" />
                 </button>
+                <h2 className="text-white text-lg font-bold"
+                    onClick={toggleSidebar}
+                >GainMate</h2>
+                </div>
             </div>
         </>
     )
